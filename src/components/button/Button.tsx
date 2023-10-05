801fdc56c361bf66ed4639d38a7210ec4595b339
AlexandrIvanov <alexandr.1996@list.ru>
2023-10-05 16:44:46 +0300

    Fix styles

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,13 +4,14 @@ import { ButtonHTMLAttributes, FC } from 'react';
 
 type Button = {
   withIcon: boolean;
+  addClass?: string;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button: FC<Button> = (props) => {
-  const { children, withIcon, ...rest } = props;
+  const { children, withIcon, addClass, ...rest } = props;
 
   return (
-    <button className={cl.button} {...rest}>
+    <button className={`${cl.button} ${addClass}`} {...rest}>
       {withIcon && <FaArrowLeftLong />}
       {children}
     </button>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import cl from './Header.module.scss';
 import clContainer from './Container.module.scss';
 import { IoMoonOutline, IoMoonSharp } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
 
 type Theme = 'light' | 'dark';
 
@@ -20,7 +21,11 @@ export const Header = () => {
     <div className={cl.header}>
       <div className={clContainer.container}>
         <div className={cl.wrapper}>
-          <h1>Where in the world?</h1>
+          <h1>
+            <Link to='/' className={cl.title}>
+              Where in the world?
+            </Link>
+          </h1>
           <div onClick={toggleTheme} className={cl.themeSwitcher}>
             {theme === 'light' ? <IoMoonOutline /> : <IoMoonSharp />}
             <div>Dark Mode</div>
